3f11327a5b645ead03b6179a62f6288e16b2416c
gme5130 <gme5130@psu.edu>
2020-09-03 18:02:00 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+// Author: Griffin Edmunds gme5130@psu.Edmunds
+// Collaborator: Julia Cable jfc5978@psu.edu
+// Collaborator: Sujay Kandwal sfk5645@psu.edu
+// Collaborator: Nicholas George ntg5067@psu.edu
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
